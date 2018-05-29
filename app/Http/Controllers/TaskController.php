06f06fd937b9097b4e0a81ecd386149834be57c4
maho.tanaka <ec2-user@ip-172-31-37-22.us-east-2.compute.internal>
2018-05-29 05:49:12 +0000

    CRUD tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -52,6 +52,7 @@ class TaskController extends Controller
             ]);
             
         $task = new Task;
+        $task->status = $request ->status;
         $task->content = $request->content;
         $task->save();
 
