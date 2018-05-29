d71a5356d06c5e55606a651dfee68f37a885b19f
maho.tanaka <ec2-user@ip-172-31-37-22.us-east-2.compute.internal>
2018-05-29 05:31:59 +0000

    validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -47,7 +47,11 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-         $task = new Task;
+        $this->validate($request,[
+            'status' =>'required|max:10',
+            ]);
+            
+        $task = new Task;
         $task->content = $request->content;
         $task->save();
 
@@ -95,7 +99,11 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-         $task = Task::find($id);
+         $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
+        $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
 
